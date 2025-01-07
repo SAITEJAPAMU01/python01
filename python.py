cabbeaf1da7e930a1d5a2399bed3203956c9c138
SAITEJAPAMU01 <saiteja.nxt01@gmail.com>
2025-01-07 23:53:50 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,26 @@
+#output operations
+print("Hello, Python!")
+#o/p: Hello, Python!
+
+
+#using sep and end parameters
+print("Hello", "Python", sep=",", end="!")
+
+#o/p: Hello,Python!
+
+#formatteing of string
+name="sai"
+age=20
+print(f"{name} is {age} years old.")
+#o/p: sai is 20 years old.
+
+#input operations
+name=input("Enter your name:")
+print("Hello", name)
+
+#converting input to others types by default it is string
+age=int(input("Enter your age:"))
+print("Your age is", age)
+
+
+
